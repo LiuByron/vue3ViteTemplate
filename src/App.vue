29119019ce7cf4bc1d74953065cc8e3f1d2365eb
zero <liujunjian007@163.com>
2024-05-11 15:31:36 +0800

    feat: 新增config控制
    新增控制

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { generateArray } from 'Utils/tools' // 使用Utils别名
+console.log(generateArray(20))
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      Assets: path.resolve(__dirname, './src/assets'),
+      Components: path.resolve(__dirname, './src/components'),
+      Utils: path.resolve(__dirname, './src/utils'), // 工具类方法（新创建的）
+    },
+  },
 })
