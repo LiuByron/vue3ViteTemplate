3dbbd52b3bc843f8c197a7522cab1b8f1452b707
zero <liujunjian007@163.com>
2024-05-23 10:03:08 +0800

    优化vite获取

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { envParse } from 'vite-plugin-env-parse'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    envParse(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
