1aeae22812ef08442700ee5dd345053e11ae4cef
zero <liujunjian007@163.com>
2024-07-26 11:39:41 +0800

    初始化提交

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -8,18 +12,14 @@
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <router-view></router-view>
 </template>
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 <style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
+  transition: filter 300ms;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue';
-import store from './store';
-import router from './router';
-import TDesign from 'tdesign-vue-next';
-import 'tdesign-vue-next/es/style/index.css';
+import App from './App.vue'
 
-const app = createApp(App);
-
-app.use(store);
-app.use(router);
-app.use(TDesign);
-app.mount('#app');
+createApp(App).mount('#app')
